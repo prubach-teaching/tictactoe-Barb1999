f3a5e44dad9df6ebfa5cacc532e6515c46515905
Barb1999 <61975391+Barb1999@users.noreply.github.com>
2020-04-27 16:31:51 +0200

    Update TicTacToe.java

--- src/main/java/sgh/TicTacToe.java
+++ src/main/java/sgh/TicTacToe.java
@@ -2,99 +2,94 @@ package sgh;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToe {
 
-    public enum Result {NOT_FINISHED, NO_WINNER, X_WON, O_WON}
+    public enum Result { NOT_FINISHED, NO_WINNER, X_WON, O_WON }
 
-    public static int checkRows(int[][] board) {
-        for (int[] row : board) {
-            if (Arrays.equals(row, new int[]{1, 1, 1})) {
-                return 1;
-            } else if (Arrays.equals(row, new int[]{-1, -1, -1})) {
-                return -1;
-            }
-        }
-        return 0;
-    }
+    public static Result checkBoard(String boardFileName) throws FileNotFoundException {
+        File boardFile = new File(boardFileName);
+        System.out.println(boardFile.getAbsolutePath());
 
-    public static int checkColumns(int[][] board) {
-        for (int mark : new int[]{-1, 1}) {
-            for (int i = 0; i < 3; i++) {
-                if (board[0][i] == mark && board[1][i] == mark && board[2][i] == mark) {
-                    return mark;
+        Scanner p = new Scanner(boardFile);
+        int kolumny = 5;
+        int wiersze = 3;
+         
+            char[][] Gra = new char[wiersze][kolumny];
+
+
+            for (int rzad = 0; p.hasNextLine() && rzad < wiersze; rzad++) {
+                char[] abc = p.nextLine().toCharArray();
+                for (int i = 0; i < kolumny && i < abc.length; i++) {
+                    Gra[rzad][i] = abc[i];
                 }
             }
-        }
-        return 0;
-    }
 
-    public static int checkDiagonals(int[][] board) {
-        for (int mark : new int[]{-1, 1}) {
-
-            if (board[0][0] == mark && board[1][1] == mark && board[2][2] == mark) {
-                return mark;
-            } else if (board[2][0] == mark && board[1][1] == mark && board[0][2] == mark) {
-                return mark;
-            }
-
-        }
-        return 0;
-    }
-
-    public static Result checkBoard(String boardFileName) {
-        int[][] board = new int[3][3];
-        try {
-            File boardFile = new File(boardFileName);
-            System.out.println(boardFile.getAbsolutePath());
-            Scanner fileReader = new Scanner(boardFile);
-            int rowCount = 0;
-            while (fileReader.hasNextLine()) {
-                String[] data = fileReader.nextLine().split(";");
-                int columnCount = 0;
-                for (String mark : data) {
-                    if (mark.equals("x")) {
-                        board[rowCount][columnCount] = 1;
-                        columnCount++;
-                    } else if (mark.equals("o")) {
-                        board[rowCount][columnCount] = -1;
-                        columnCount++;
-                    } else {
-                        board[rowCount][columnCount] = 0;
-                        columnCount++;
+            for (int m = 0; m < wiersze; m++) {
+                for (int n = 0; n < kolumny; n++) {
+                    if (Gra[m][n] == 'x') {
+                    	Gra[m][n] = 1;
+                    }
+                    else if (Gra[m][n] == 'o') {
+                    		Gra[m][n] = 100;
+                    }
+                    else if (Gra[m][n] == ';') {
+                    	    Gra[m][n] = 0;
                     }
                 }
-                rowCount++;
+
             }
-        } catch (FileNotFoundException exception) {
-            System.out.println("Exception occured");
-            exception.printStackTrace();
-        }
 
-
-        if (checkRows(board) == 1 || checkColumns(board) == 1 || checkDiagonals(board) == 1) {
-            return Result.X_WON;
-        } else if (checkRows(board) == -1 || checkColumns(board) == -1 || checkDiagonals(board) == -1) {
-            return Result.O_WON;
-        }
-
-        for (int[] row : board){
-            for (int mark:row){
-                if(mark == 0){
-                    return Result.NOT_FINISHED;
+            //check rows for winning
+            for (int m=0; m<wiersze; m++) {
+                if ((Gra[m][0] + Gra[m][2] + Gra[m][4]) == 3) {
+                    return Result.X_WON;
+                }
+                else if ((Gra[m][0] + Gra[m][2] + Gra[m][4]) == 300) {
+                    return Result.O_WON;
                 }
             }
-        }
 
+            //check columns for winning
+            for (int n=0; n<kolumny; n++) {
+                if ((Gra[0][n] + Gra[1][n] + Gra[2][n]) == 3) {
+                    return Result.X_WON;
+                }
+                else if ((Gra[0][n] + Gra[1][n] + Gra[2][n]) == 300) {
+                    return Result.O_WON;
+                }
+            }
+
+            //check 1st diagonal for winning
+            if ((Gra[0][0] + Gra[1][2] + Gra[2][4]) == 3) {
+                return Result.X_WON;
+            } else if ((Gra[0][0] + Gra[1][2] + Gra[2][4]) == 300) {
+                return Result.O_WON;
+            }
+
+            //check 2nd diagonal for winning
+            if ((Gra[0][4] + Gra[1][2] + Gra[2][0]) == 3) {
+                return Result.X_WON;
+            } else if ((Gra[0][4] + Gra[1][2] + Gra[2][0]) == 300) {
+                return Result.O_WON;
+            }
+
+            //check for empty spaces
+            for (int m = 0; m < wiersze; m++) {
+                
+                for (int n = 0; n < kolumny; n++) {
+
+                    if (Gra[m][n] != 1 && Gra[m][n] != 100 && Gra[m][n] != 0) {
+                        return Result.NOT_FINISHED;
+                    }
+                }
+            }
+        
         return Result.NO_WINNER;
     }
-
-
-    public static void main(String[] args) {
-        Result res = checkBoard("boards/tick3.csv");
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        Result res = checkBoard("boards/tick0.csv");
         System.out.println(res);
     }
-}
